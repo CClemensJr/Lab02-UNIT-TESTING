db19ed004cc4977b6935167b6ef6bcf4f893b737
Charles Clemens <cclemensjr@gmail.com>
2019-01-09 13:39:15 -0800

    Created method for formatting menu into columns

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -28,16 +28,22 @@ namespace ATM
 
         static void ShowMenu()
         {
-            Console.WriteLine("Please select an option:\n\n");
-            Console.WriteLine("1. View your balance.");
-            Console.WriteLine("2. Withdraw cash.");
-            Console.WriteLine("3. Deposit cash.");
+            CenterText("Please select an option:\n\n");
+            ColumnizeText("1.",  "View your balance.");
+            ColumnizeText("2.", "Withdraw cash.");
+            ColumnizeText("3.", "Deposit cash.");
         }
 
         // CenterText will center text in the console window. Inspiration came from MSDN docs on String.Format and this stackoverflow article: stackoverflow.com/questions/21917203/how-do-i-center-text-in-a-console-application
         static void CenterText(string text)
         {
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
+        }
+
+        // ColumnizeText is similar to center text except it will center the text across two columns
+        static void ColumnizeText(string menuSelector, string selectorText)
+        {
+            Console.WriteLine(menuSelector + selectorText);
         }
     }
 }
