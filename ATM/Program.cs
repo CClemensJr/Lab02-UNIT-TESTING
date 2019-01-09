3d8eec071e25eb3d2e190bd8766c761b87d20117
Charles Clemens <cclemensjr@gmail.com>
2019-01-09 14:59:29 -0800

    Refactored CenterText and deleted Columnize

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -37,15 +37,15 @@ namespace ATM
         {
             CenterText("Please select an option:");
             CenterText("------------------------");
-            ColumnizeText("1.",  "View your balance.");
-            ColumnizeText("2.", "Withdraw cash.");
-            ColumnizeText("3.", "Deposit cash.");
+            CenterText("1.    View your balance.");
+            CenterText("2.        Withdraw cash.");
+            CenterText("3.         Deposit cash.");
         }
 
         // Collect the user input
         static void GetMenuSelection()
         {
-            CenterText("Your Selection: ");
+            CenterText("Your Selection: ", 1);
 
             string menuSelector = Console.ReadLine();
         }
@@ -65,17 +65,16 @@ namespace ATM
         }
 
         // CenterText will center text in the console window. Inspiration came from MSDN docs on String.Format and this stackoverflow article: stackoverflow.com/questions/21917203/how-do-i-center-text-in-a-console-application
-        static void CenterText(string text)
+        static void CenterText(string text, int writeLine = 0)
         {
-            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
+            if (writeLine == 0)
+            {
+                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
+            }
+            else
+            {
+                Console.Write(String.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
+            }
         }
-
-        // ColumnizeText is similar to center text except it will center the text across two columns
-        static void ColumnizeText(string menuSelector, string selectorText)
-        {
-            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 3) + (menuSelector.Length)) + "} {1," + ((Console.WindowWidth / 4) + (selectorText.Length)) + "}", menuSelector, selectorText));
-        }
-
-
     }
 }
