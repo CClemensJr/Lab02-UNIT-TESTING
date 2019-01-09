1276d11113b43077f02f6230fa40ee52a8aa3a1c
Charles Clemens <cclemensjr@gmail.com>
2019-01-09 13:30:20 -0800

    Created function responsible for showing heading

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -12,15 +12,23 @@ namespace ATM
             Console.ReadLine();
         }
 
+        // The userinterface will interact with the user
         static void UserInterface()
         {
-            CenterText("Welcome to Code Fellows Credit Union.");
-            CenterText("Thank you for your membership!");
+            ShowHeading();
         }
 
+        // Show heading is responsible for showing the credit union slogan
+        static void ShowHeading()
+        {
+            CenterText("Welcome to Code Fellows Credit Union.\n");
+            CenterText("Thank you for your membership!\n\n\n");
+        }
+
+        // CenterText will center text in the console window. Inspiration came from MSDN docs on String.Format and this stackoverflow article: stackoverflow.com/questions/21917203/how-do-i-center-text-in-a-console-application
         static void CenterText(string text)
         {
-            Console.WriteLine(text);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
         }
     }
 }
