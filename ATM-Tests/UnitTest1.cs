ec7604549098d1defd38f55043609401f5991eed
CClemensJr <cclemensjr@gmail.com>
2019-01-09 23:41:28 -0800

    Added more tests for wthdraw and deposit methods

--- ATM-Tests/UnitTest1.cs
+++ ATM-Tests/UnitTest1.cs
@@ -16,6 +16,26 @@ namespace ATM_Tests
         }
 
         [Fact]
+        public void WithdrawalsShouldSubtractFromBalance()
+        {
+            decimal balance = 10.00m;
+            decimal withdrawal = 5.00m;
+            decimal newBalance = balance - withdrawal;
+
+            Assert.Equal(newBalance, Program.WithdrawCash(balance, withdrawal));
+        }
+
+        [Fact]
+        public void WithdrawalsShouldNotAddToBalance()
+        {
+            decimal balance = 10.00m;
+            decimal withdrawal = 5.00m;
+            decimal newBalance = balance + withdrawal;
+
+            Assert.NotEqual(newBalance, Program.WithdrawCash(balance, withdrawal));
+        }
+
+        [Fact]
         public void DepositsShouldAddToBalance()
         {
             decimal balance = 0.00m;
@@ -23,6 +43,16 @@ namespace ATM_Tests
             decimal newBalance = balance + deposit;
 
             Assert.Equal(newBalance, Program.DepositCash(balance, deposit));
+        }
+
+        [Fact]
+        public void DepositsShouldNotSubtractFromBalance()
+        {
+            decimal balance = 0.00m;
+            decimal deposit = 5.00m;
+            decimal newBalance = balance - deposit;
+
+            Assert.NotEqual(newBalance, Program.DepositCash(balance, deposit));
         }
     }
 }
